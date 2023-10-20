17f7ed621439deacc6d6774991509ecc78938cfd
Joelle  Johnson <joellej221@gmail.com>
2023-10-20 14:25:00 -0400

    created api that connects to database

---
+++ functions/index.js
@@ -0,0 +1,15 @@
+import db from './src/connectDb.js';
+import express from 'express';
+import cors from 'cors';
+import { getAllPets, addPet } from './src/pets.js';
+import { onRequest } from 'firebase-functions/v2/https';
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/pets", getAllPets)
+app.post("/pets", addPet)
+
+export const api = onRequest(app)
